18cea225260fcaf89042e0f13694ccdf504a5283
vishesh32 <wish.mongia@gmail.com>
2025-01-04 03:31:28 +0000

    simple ReLu implementation

--- ReLu.sv
+++ ReLu.sv
@@ -1,9 +1,17 @@
-module ReLu #(parameter )
+module ReLu #(parameter DATA_WIDTH = 32)
 (
     input clk,
-    input activation_function,
+    input [DATA_WIDTH-1:0] x,
+    output reg [DATA_WIDTH-1:0] out
 
 );
 
-  
+always @(posedge clk)
+begin
+    if ($signed(x) > 0)
+        out <= x;
+    else
+        out <= 0;
+end
       
+endmodule
